0ca0736bdb0fa2461e116f467c9a8b4688c3d818
notchmarketing25-wq <notchmarketing25@gmail.com>
2025-08-22 04:41:58 +0300

    Add shopzawy-clone/.next/static/chunks/src_app_dashboard_products_page_tsx_f3db7405._.js 1755826917455

---
+++ shopzawy-clone/.next/static/chunks/src_app_dashboard_products_page_tsx_f3db7405._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_dashboard_products_page_tsx_5c10633e._.js"
+],
+    source: "dynamic"
+});
